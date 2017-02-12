6afabf2fc6cc8674ef9600837ab76364cb04a8a0
Gareth Stephenson <gareth.stephenson@gmail.com>
2017-02-12 20:48:01 +0200

    Added routing options for pis

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var pis = require('./routes/pis');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/pis', pis);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
